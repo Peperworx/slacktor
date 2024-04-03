136fb2f8e4dc568bfbab77cc48ed4aa2a528ccb6
Riley Wilton <30930649+wireboy5@users.noreply.github.com>
2024-04-03 17:17:22 -0400

    Readme, move main to an example.

--- examples/simple.rs
+++ examples/simple.rs
@@ -30,17 +30,18 @@ fn main() {
     let mut system = Slacktor::new();
 
     // Create a new actor
-    let actor_id = system.spawn(TestActor(rand::random::<u64>()));
+    let actor_id = system.spawn(TestActor(10));
 
+    // Get a reference to the actor
     let a = system.get::<TestActor>(actor_id).unwrap();
 
     // Time 1 billion messages, appending each to a vector and doing some math to prevent the
-    // code being completely optimzied away. (Which is impressive that the compiler can do.)
-    // On my machine, I am averaging 600-700 million messages/second.
+    // code being completely optimzied away.
     let num_messages = 1_000_000_000;
     let mut out = Vec::with_capacity(num_messages);
     let start = Instant::now();
     for i in 0..num_messages {
+        // Send the message
         let v = a.send(TestMessage(i as u64));
         out.push(v);
     }
@@ -49,17 +50,6 @@ fn main() {
         "{:.2} messages/sec",
         num_messages as f64 / elapsed.as_secs_f64()
     );
-
-    // With no storing it to the vector, rustc can optimize everything away
-    // This doesn't happen with other actor frameworks.
-    // Sadly, this optimization does not occur when using tokio, async std, or smol.
-    let num_messages: u64 = 1_000_000_000;
-    let start = Instant::now();
-    for i in 0..num_messages {
-        let _v = a.send(TestMessage(i as u64));
-    }
-    let elapsed = start.elapsed();
-    println!("{:.2} m/s", num_messages as f64 / elapsed.as_secs_f64());
 
     system.kill(actor_id);
 }
