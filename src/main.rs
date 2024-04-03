3b6a52a34de0d893b719e0696d0311f3b194c032
Riley Wilton <30930649+wireboy5@users.noreply.github.com>
2024-04-03 16:28:03 -0400

    Blazingly fast(tm) with no async. Still super fast with async, but only on the order of 300-400 million messages per second depending on the executor, roughly 800 million per second with noo appending to a vector. Still, with no async, it is able to pretty much completely optimize away the actor system, which is great.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::time::Instant;
 
-use slacktor::{actor::{Handler, Message}, Slacktor};
+use slacktor::{
+    actor::{Handler, Message},
+    Slacktor,
+};
 
 struct TestMessage(pub u64);
 
@@ -8,16 +11,13 @@ impl Message for TestMessage {
     type Result = u64;
 }
 
-
 struct TestActor(pub u64);
-
 
 impl Handler<TestMessage> for TestActor {
     fn handle_message(&self, m: TestMessage) -> u64 {
-        m.0^self.0
+        m.0 ^ self.0
     }
 }
-
 
 fn main() {
     // Create a slacktor instance
@@ -39,15 +39,19 @@ fn main() {
         out.push(v);
     }
     let elapsed = start.elapsed();
-    println!("{:.2} messages/sec", num_messages as f64/elapsed.as_secs_f64());
+    println!(
+        "{:.2} messages/sec",
+        num_messages as f64 / elapsed.as_secs_f64()
+    );
 
     // With no storing it to the vector, rustc can optimize everything away
     // This doesn't happen with other actor frameworks.
+    // Sadly, this optimization does not occur when using tokio, async std, or smol.
     let num_messages: u64 = 1_000_000_000;
     let start = Instant::now();
     for i in 0..num_messages {
         let _v = a.send(TestMessage(i as u64));
     }
     let elapsed = start.elapsed();
-    println!("{:?}", elapsed);
+    println!("{:.2} m/s", num_messages as f64 / elapsed.as_secs_f64());
 }
